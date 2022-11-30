305ae6890ff75f93e286a4b4beedb1545c5891c9
bluedragon8633 <94876427+bluedragon8633@users.noreply.github.com>
2022-11-30 08:22:17 -0500

    Update main.c

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ struct Coords {
 };
 
 struct Player {
-	Coords pos = {0,0};
+	struct Coords pos;
 };
 
 void drawScreen() {
@@ -16,13 +16,27 @@ void drawScreen() {
 	for (int x = 0; x < 25; x++) {
 		for (int y = 0; y < 15; y++) {
 			if (screenArray[x][y] == 0) {
-				printf("");
+				printf("\x1b[0m  ");
 			} else if (screenArray[x][y] == 1) {
 				printf("\x1b[37m  \x1b[0m");
-			} else if (screeenArray[x][y] == 2) {
+			} else if (screenArray[x][y] == 2) {
 				printf("\x1b[31m  \x1b[0m");
 			}
 		}
 		printf("\n");
 	}
+}
+
+
+int main() {
+    //while (1) {
+    struct Player player;
+    player.pos.x = 3;
+    player.pos.y = 8;
+    screenArray[player.pos.x][player.pos.y] = 1;
+    screenArray[0][0] = 2;
+    drawScreen();
+    //}
+
+    return 1;   
 }
